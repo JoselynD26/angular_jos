e5fcad779e26e63f72040d61ad04807decbd6062
JoselynD26 <jmr.dicao@yavirac.edu.ec>
2024-05-31 09:09:06 -0500

    clase 13

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  private http=inject(HttpClient)
+
+  loginForm= new FormGroup({
+    email:new FormControl("", [Validators.email, Validators.required]),
+    password:new FormControl("", Validators.required)
+  })
+  funIngresar(){
+    this.http.get("http://jsonplaceholder.typicode.com/todos").subscribe(
+      (res)=>{
+        console.log(res)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
+  }
+
+}
