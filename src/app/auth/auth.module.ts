ed3077a049749cba2497b8c12cb9a6396abc495a
JoselynD26 <jmr.dicao@yavirac.edu.ec>
2024-06-03 00:11:47 -0500

    clase13
    jos 3/6/2024

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,8 +6,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import {ReactiveFormsModule} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
+  providers:[
+    AuthService
+  ],
   declarations: [
     LoginComponent,
     RegisterComponent
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl,FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,14 +9,14 @@ import { FormControl,FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
-  private http=inject(HttpClient)
+  private authService=inject(AuthService)
 
   loginForm= new FormGroup({
     email:new FormControl("", [Validators.email, Validators.required]),
     password:new FormControl("", Validators.required)
   })
   funIngresar(){
-    this.http.get("http://jsonplaceholder.typicode.com/todos").subscribe(
+    this.authService.loginConNest(this.loginForm.value).subscribe(
       (res)=>{
         console.log(res)
       },
